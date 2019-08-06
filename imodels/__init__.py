ce0f7821230d9928c4967b8ac4afb342dfa47ce8
Chandan Singh <chandan_singh@berkeley.edu>
2019-08-06 04:11:40 -0700

    fix __init__

--- imodels/__init__.py
+++ imodels/__init__.py
@@ -1,4 +1,4 @@
 from .bayesian_rule_lists.RuleListClassifier import RuleListClassifier
-from .rulefit.ruleft import RuleFit
+from .rulefit.rulefit import RuleFit
 # from .slim import SLIM
 # from .optimal_classification_tree import OptimalTreeModel
