92b7fad763e8af15e5ea3b3fe69f8a78f4d921d5
Chandan Singh <chandan_singh@berkeley.edu>
2020-07-10 10:47:45 -0700

    add SLIM properly

--- imodels/slim/SLIM.py
+++ imodels/slim/SLIM.py
@@ -1,6 +1,6 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
-import cvxpy # package for optimization
+import cvxpy as cp # package for optimization
 
 class SLIM():
     def __init__(self):
@@ -9,21 +9,28 @@ class SLIM():
     
     
     def fit(self, X, y, lambda_reg=0):
-        '''fit a linear model with integer coefficient and L1 regularizaiton'''
-
+        '''fit a linear model with integer coefficient and L1 regularization
+        '''
+        if 'pandas' in str(type(X)):
+            X = X.values
+        if 'pandas' in str(type(y)):
+            y = y.values
+        assert type(X) == np.ndarray, 'inputs should be ndarrays'
+        assert type(y) == np.ndarray, 'inputs should be ndarrays'
+        
         # declare the integer-valued optimization variable
-        w = cvxpy.Variable(X.shape[1], integer=True)
+        w = cp.Variable(X.shape[1], integer=True)
+        
 
         # set up the minimization problem
-        obj = cvxpy.Minimize(cvxpy.norm(X * w - y, 2) + lambda_reg * cvxpy.norm(w, 1))
-        prob = cvxpy.Problem(obj)
+        mse = cp.sum_squares(X @ w - y)
+        l1_penalty = lambda_reg * cp.norm(w, 1)
+        obj = cp.Minimize(mse + l1_penalty)
+        prob = cp.Problem(obj)
 
         # solve the problem using an appropriate solver
-        sol = prob.solve(solver = 'ECOS_BB')
-
-        # the optimal value
+        sol = prob.solve()
         self.model.coef_ = w.value.astype(np.int)
         self.model.intercept_ = 0
-        self.coef_ = self.model.coef_
         
         
