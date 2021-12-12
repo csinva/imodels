dc06dcdb2a84ea69d8f5f93efb3f24c97ba512d8
aagarwal1996 <aa3797@berkeley.edu>
2021-12-13 07:05:47 +0800

    adding var args in order to vary complexity in imodels experiments

--- imodels/tree/shrunk_tree.py
+++ imodels/tree/shrunk_tree.py
@@ -110,8 +110,8 @@ class ShrunkTreeClassifier(ShrunkTree):
 
 class ShrunkTreeClassifierCV(ShrunkTreeClassifier):
     def __init__(self, estimator_: BaseEstimator,
-                 reg_param_list: List[float] = [0.1, 1, 10, 50, 100, 500],
-                 cv: int = 3, scoring=None):
+                 reg_param_list: List[float] = [0.1, 1, 10, 50, 100, 500], max_leaf_nodes: int = None,
+                 cv: int = 3, scoring=None,*args,**kwargs):
         super().__init__(estimator_, reg_param=None)
         self.reg_param_list = np.array(reg_param_list)
         self.cv = cv
@@ -125,7 +125,7 @@ class ShrunkTreeClassifierCV(ShrunkTreeClassifier):
     def fit(self, X, y, *args, **kwargs):
         self.scores_ = []
         for reg_param in self.reg_param_list:
-            est = ShrunkTreeClassifier(deepcopy(self.estimator_), reg_param)
+            est = ShrunkTreeClassifier(deepcopy(self.estimator_), reg_param,max_leaf_nodes = max_leaf_nodes)
             cv_scores = cross_val_score(est, X, y, cv=self.cv, scoring=self.scoring)
             self.scores_.append(np.mean(cv_scores))
         self.reg_param = self.reg_param_list[np.argmax(self.scores_)]
@@ -135,7 +135,7 @@ class ShrunkTreeClassifierCV(ShrunkTreeClassifier):
 class ShrunkTreeRegressorCV(ShrunkTreeRegressor):
     def __init__(self, estimator_: BaseEstimator,
                  reg_param_list: List[float] = [0.1, 1, 10, 50, 100, 500],
-                 cv: int = 3, scoring=None):
+                 cv: int = 3, scoring=None,*args,**kwargs):
         super().__init__(estimator_, reg_param=None)
         self.reg_param_list = np.array(reg_param_list)
         self.cv = cv
@@ -146,7 +146,7 @@ class ShrunkTreeRegressorCV(ShrunkTreeRegressor):
         #     raise Warning('Passed an already fitted estimator,'
         #                   'but shrinking not applied until fit method is called.')
 
-    def fit(self, X, y, *args, **kwargs):
+    def fit(self, X, y):
         self.scores_ = []
         for reg_param in self.reg_param_list:
             est = ShrunkTreeRegressor(deepcopy(self.estimator_), reg_param)
@@ -182,8 +182,8 @@ class ShrunkTreeRegressorCV(ShrunkTreeRegressor):
 
 if __name__ == '__main__':
     np.random.seed(15)
-    X, y = datasets.fetch_california_housing(return_X_y=True)  # regression
-    # X, y = datasets.load_breast_cancer(return_X_y=True)  # binary classification
+    #X, y = datasets.fetch_california_housing(return_X_y=True)  # regression
+    X, y = datasets.load_breast_cancer(return_X_y=True)  # binary classification
     # X, y = datasets.load_diabetes(return_X_y=True)  # regression
     # X = np.random.randn(500, 10)
     # y = (X[:, 0] > 0).astype(float) + (X[:, 1] > 1).astype(float)
@@ -205,8 +205,8 @@ if __name__ == '__main__':
 
     # m = ShrunkTree(estimator_=DecisionTreeRegressor(random_state=42, max_features=None), reg_param=10)
     # m = ShrunkTree(estimator_=DecisionTreeClassifier(random_state=42, max_features=None), reg_param=0)
-    m = ShrunkTreeRegressorCV(estimator_=DecisionTreeRegressor(max_leaf_nodes=20, random_state=42),
-                              reg_param_list=[0.1, 1, 10, 100])
+    m = ShrunkTreeRegressorCV(estimator_=DecisionTreeRegressor(max_leaf_nodes=20, random_state=42), 
+                              reg_param_list=[0.1, 1, 10, 100], max_leaf_nodes = 20)
     # m = ShrunkTreeCV(estimator_=DecisionTreeClassifier())
 
     m.fit(X_train, y_train)
