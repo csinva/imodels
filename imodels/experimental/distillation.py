67e7a2ae9474cd35061b481a70118ef661908608
Chandan Singh <csinva@yahoo.com>
2022-01-16 14:48:59 -0800

    Shrinkage distillation (#78)
    * Add distillation
    
    * Add distillation
    
    Authored-by: Yan Shuo Tan <yanshuo@gmail.com>

---
+++ imodels/experimental/distillation.py
@@ -0,0 +1,50 @@
+import numpy as np
+from typing import List
+
+from sklearn.base import RegressorMixin, BaseEstimator, is_regressor
+
+class DistilledModel(BaseEstimator, RegressorMixin):
+    """
+    Experimental class to implement distillation.
+    """
+
+    def __init__(self, teacher: BaseEstimator, student : BaseEstimator):
+        self.teacher = teacher
+        self.student = student
+        self._validate_student()
+        self._check_teacher_type()
+
+    def _validate_student(self):
+        if is_regressor(self.student):
+            pass
+        else:
+            if not hasattr(self.student, "prediction_task"):
+                raise ValueError("Student must be either a scikit-learn or imodels regressor")
+            elif self.student.prediction_task == "classification":
+                raise ValueError("Student must be a regressor")
+
+    def _check_teacher_type(self):
+        if hasattr(self.teacher, "prediction_task"):
+            self.teacher_type = self.teacher.prediction_task
+        elif hasattr(self.teacher, "_estimator_type"):
+            if is_regressor(self.teacher):
+                self.teacher_type = "regression"
+            else:
+                self.teacher_type = "classification"
+
+    def set_teacher_params(self, **params):
+        self.teacher.set_params(**params)
+
+    def set_student_params(self, **params):
+        self.student.set_params(**params)
+
+    def fit(self, X, y, **kwargs):
+        self.teacher.fit(X, y, **kwargs)
+        if self.teacher_type == "regression":
+            preds = self.teacher.predict(X)
+        else:
+            preds = self.teacher.predict_proba(X)[:,1]
+        self.student.fit(X, preds)
+
+    def predict(self, X):
+        return self.student.predict(X)
