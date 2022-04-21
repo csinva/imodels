c75b8326f85e0ab3d971bc9eff8ce2cc8fb12d7d
Tiffany Tang <tiffany.tang@berkeley.edu>
2022-04-21 16:24:35 -0700

    copy of figs

---
+++ imodels/tree/anova-figs.py
@@ -0,0 +1,393 @@
+from copy import deepcopy
+from typing import List
+
+import numpy as np
+import sklearn.datasets
+from sklearn import datasets
+from sklearn import tree
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.utils import check_X_y, check_array
+
+
+class Node:
+    def __init__(self, feature: int = None, threshold: int = None,
+                 value=None, idxs=None, is_root: bool = False, left=None,
+                 impurity_reduction: float = None, tree_num: int = None,
+                 right=None):
+        """Node class for splitting
+        """
+
+        # split or linear
+        self.is_root = is_root
+        self.idxs = idxs
+        self.tree_num = tree_num
+        self.feature = feature
+        self.impurity_reduction = impurity_reduction
+
+        # different meanings
+        self.value = value  # for split this is mean, for linear this is weight
+
+        # split-specific
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.left_temp = None
+        self.right_temp = None
+
+    def setattrs(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __str__(self):
+        if self.is_root:
+            return f'X_{self.feature} <= {self.threshold:0.3f} (Tree #{self.tree_num} root)'
+        elif self.left is None and self.right is None:
+            return f'Val: {self.value[0][0]:0.3f} (leaf)'
+        else:
+            return f'X_{self.feature} <= {self.threshold:0.3f} (split)'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class FIGS(BaseEstimator):
+    """FIGS (sum of trees) classifier.
+    Fast Interpretable Greedy-Tree Sums (FIGS) is an algorithm for fitting concise rule-based models.
+    Specifically, FIGS generalizes CART to simultaneously grow a flexible number of trees in a summation.
+    The total number of splits across all the trees can be restricted by a pre-specified threshold, keeping the model interpretable.
+    Experiments across real-world datasets show that FIGS achieves state-of-the-art prediction performance when restricted to just a few splits (e.g. less than 20).
+    https://arxiv.org/abs/2201.11931
+    """
+
+    def __init__(self, max_rules: int = 12, min_impurity_decrease: float = 0.0):
+        super().__init__()
+        self.max_rules = max_rules
+        self.min_impurity_decrease = min_impurity_decrease
+        self._init_prediction_task()  # decides between regressor and classifier
+        self._init_decision_function()
+
+    def _init_prediction_task(self):
+        """
+        FIGSRegressor and FIGSClassifier override this method
+        to alter the prediction task. When using this class directly,
+        it is equivalent to FIGSRegressor
+        """
+        self.prediction_task = 'regression'
+
+    def _init_decision_function(self):
+        """Sets decision function based on prediction_task
+        """
+        # used by sklearn GrriidSearchCV, BaggingClassifier
+        if self.prediction_task == 'classification':
+            decision_function = lambda x: self.predict_proba(x)[:, 1]
+        elif self.prediction_task == 'regression':
+            decision_function = self.predict
+
+    def _construct_node_with_stump(self, X, y, idxs, tree_num, sample_weight=None):
+        # array indices
+        SPLIT = 0
+        LEFT = 1
+        RIGHT = 2
+
+        # fit stump
+        stump = tree.DecisionTreeRegressor(max_depth=1)
+        if sample_weight is not None:
+            sample_weight = sample_weight[idxs]
+        stump.fit(X[idxs], y[idxs], sample_weight=sample_weight)
+
+        # these are all arrays, arr[0] is split node
+        # note: -2 is dummy
+        feature = stump.tree_.feature
+        threshold = stump.tree_.threshold
+
+        impurity = stump.tree_.impurity
+        n_node_samples = stump.tree_.n_node_samples
+        value = stump.tree_.value
+
+        # no split
+        if len(feature) == 1:
+            # print('no split found!', idxs.sum(), impurity, feature)
+            return Node(idxs=idxs, value=value[SPLIT], tree_num=tree_num,
+                        feature=feature[SPLIT], threshold=threshold[SPLIT],
+                        impurity_reduction=None)
+
+        # split node
+        impurity_reduction = (
+                                     impurity[SPLIT] -
+                                     impurity[LEFT] * n_node_samples[LEFT] / n_node_samples[SPLIT] -
+                                     impurity[RIGHT] * n_node_samples[RIGHT] / n_node_samples[SPLIT]
+                             ) * idxs.sum()
+
+        node_split = Node(idxs=idxs, value=value[SPLIT], tree_num=tree_num,
+                          feature=feature[SPLIT], threshold=threshold[SPLIT],
+                          impurity_reduction=impurity_reduction)
+        # print('\t>>>', node_split, 'impurity', impurity, 'num_pts', idxs.sum(), 'imp_reduc', impurity_reduction)
+
+        # manage children
+        idxs_split = X[:, feature[SPLIT]] <= threshold[SPLIT]
+        idxs_left = idxs_split & idxs
+        idxs_right = ~idxs_split & idxs
+        node_left = Node(idxs=idxs_left, value=value[LEFT], tree_num=tree_num)
+        node_right = Node(idxs=idxs_right, value=value[RIGHT], tree_num=tree_num)
+        node_split.setattrs(left_temp=node_left, right_temp=node_right, )
+        return node_split
+
+    def fit(self, X, y=None, feature_names=None, verbose=False, sample_weight=None):
+        """
+        Params
+        ------
+        _sample_weight: array-like of shape (n_samples,), default=None
+            Sample weights. If None, then samples are equally weighted.
+            Splits that would create child nodes with net zero or negative weight
+            are ignored while searching for a split in each node.
+        """
+        # X, y = check_X_y(X, y)
+        y = y.astype(float)
+        if feature_names is not None:
+            self.feature_names_ = feature_names
+
+        self.trees_ = []  # list of the root nodes of added trees
+        self.complexity_ = 0  # tracks the number of rules in the model
+        y_predictions_per_tree = {}  # predictions for each tree
+        y_residuals_per_tree = {}  # based on predictions above
+
+        # set up initial potential_splits
+        # everything in potential_splits either is_root (so it can be added directly to self.trees_)
+        # or it is a child of a root node that has already been added
+        idxs = np.ones(X.shape[0], dtype=bool)
+        node_init = self._construct_node_with_stump(X=X, y=y, idxs=idxs, tree_num=-1, sample_weight=sample_weight)
+        potential_splits = [node_init]
+        for node in potential_splits:
+            node.setattrs(is_root=True)
+        potential_splits = sorted(potential_splits, key=lambda x: x.impurity_reduction)
+
+        # start the greedy fitting algorithm
+        finished = False
+        while len(potential_splits) > 0 and not finished:
+            # print('potential_splits', [str(s) for s in potential_splits])
+            split_node = potential_splits.pop()  # get node with max impurity_reduction (since it's sorted)
+
+            # don't split on node
+            if split_node.impurity_reduction < self.min_impurity_decrease:
+                finished = True
+                break
+
+            # split on node
+            if verbose:
+                print('\nadding ' + str(split_node))
+            self.complexity_ += 1
+
+            # if added a tree root
+            if split_node.is_root:
+
+                # start a new tree
+                self.trees_.append(split_node)
+
+                # update tree_num
+                for node_ in [split_node, split_node.left_temp, split_node.right_temp]:
+                    if node_ is not None:
+                        node_.tree_num = len(self.trees_) - 1
+
+                # add new root potential node
+                node_new_root = Node(is_root=True, idxs=np.ones(X.shape[0], dtype=bool),
+                                     tree_num=-1)
+                potential_splits.append(node_new_root)
+
+            # add children to potential splits
+            # assign left_temp, right_temp to be proper children
+            # (basically adds them to tree in predict method)
+            split_node.setattrs(left=split_node.left_temp, right=split_node.right_temp)
+
+            # add children to potential_splits
+            potential_splits.append(split_node.left)
+            potential_splits.append(split_node.right)
+
+            # update predictions for altered tree
+            for tree_num_ in range(len(self.trees_)):
+                y_predictions_per_tree[tree_num_] = self._predict_tree(self.trees_[tree_num_], X)
+            y_predictions_per_tree[-1] = np.zeros(X.shape[0])  # dummy 0 preds for possible new trees
+
+            # update residuals for each tree
+            # -1 is key for potential new tree
+            for tree_num_ in list(range(len(self.trees_))) + [-1]:
+                y_residuals_per_tree[tree_num_] = deepcopy(y)
+
+                # subtract predictions of all other trees
+                for tree_num_other_ in range(len(self.trees_)):
+                    if not tree_num_other_ == tree_num_:
+                        y_residuals_per_tree[tree_num_] -= y_predictions_per_tree[tree_num_other_]
+
+            # recompute all impurities + update potential_split children
+            potential_splits_new = []
+            for potential_split in potential_splits:
+                y_target = y_residuals_per_tree[potential_split.tree_num]
+
+                # re-calculate the best split
+                potential_split_updated = self._construct_node_with_stump(X=X,
+                                                                          y=y_target,
+                                                                          idxs=potential_split.idxs,
+                                                                          tree_num=potential_split.tree_num,
+                                                                          sample_weight=sample_weight, )
+
+                # need to preserve certain attributes from before (value at this split + is_root)
+                # value may change because residuals may have changed, but we want it to store the value from before
+                potential_split.setattrs(
+                    feature=potential_split_updated.feature,
+                    threshold=potential_split_updated.threshold,
+                    impurity_reduction=potential_split_updated.impurity_reduction,
+                    left_temp=potential_split_updated.left_temp,
+                    right_temp=potential_split_updated.right_temp,
+                )
+
+                # this is a valid split
+                if potential_split.impurity_reduction is not None:
+                    potential_splits_new.append(potential_split)
+
+            # sort so largest impurity reduction comes last (should probs make this a heap later)
+            potential_splits = sorted(potential_splits_new, key=lambda x: x.impurity_reduction)
+            if verbose:
+                print(self)
+            if self.max_rules is not None and self.complexity_ >= self.max_rules:
+                finished = True
+                break
+        return self
+
+    def _tree_to_str(self, root: Node, prefix=''):
+        if root is None:
+            return ''
+        elif root.threshold is None:
+            return ''
+        pprefix = prefix + '\t'
+        return prefix + str(root) + '\n' + self._tree_to_str(root.left, pprefix) + self._tree_to_str(root.right,
+                                                                                                     pprefix)
+
+    def __str__(self):
+        s = '------------\n' + '\n\t+\n'.join([self._tree_to_str(t) for t in self.trees_])
+        if hasattr(self, 'feature_names_') and self.feature_names_ is not None:
+            for i in range(len(self.feature_names_))[::-1]:
+                s = s.replace(f'X_{i}', self.feature_names_[i])
+        return s
+
+    def predict(self, X):
+        X = check_array(X)
+        preds = np.zeros(X.shape[0])
+        for tree in self.trees_:
+            preds += self._predict_tree(tree, X)
+        if self.prediction_task == 'regression':
+            return preds
+        elif self.prediction_task == 'classification':
+            return (preds > 0.5).astype(int)
+
+    def predict_proba(self, X):
+        X = check_array(X)
+        if self.prediction_task == 'regression':
+            return NotImplemented
+        preds = np.zeros(X.shape[0])
+        for tree in self.trees_:
+            preds += self._predict_tree(tree, X)
+        preds = np.clip(preds, a_min=0., a_max=1.)  # constrain to range of probabilities
+        return np.vstack((1 - preds, preds)).transpose()
+
+    def _predict_tree(self, root: Node, X):
+        """Predict for a single tree
+        """
+
+        def _predict_tree_single_point(root: Node, x):
+            if root.left is None and root.right is None:
+                return root.value
+            left = x[root.feature] <= root.threshold
+            if left:
+                if root.left is None:  # we don't actually have to worry about this case
+                    return root.value
+                else:
+                    return _predict_tree_single_point(root.left, x)
+            else:
+                if root.right is None:  # we don't actually have to worry about this case
+                    return root.value
+                else:
+                    return _predict_tree_single_point(root.right, x)
+
+        preds = np.zeros(X.shape[0])
+        for i in range(X.shape[0]):
+            preds[i] = _predict_tree_single_point(root, X[i])
+        return preds
+
+
+class FIGSRegressor(FIGS):
+    def _init_prediction_task(self):
+        self.prediction_task = 'regression'
+
+
+class FIGSClassifier(FIGS):
+    def _init_prediction_task(self):
+        self.prediction_task = 'classification'
+
+
+class FIGSCV:
+    def __init__(self, figs,
+                 n_rules_list: List[float] = [6, 12, 24, 30, 50],
+                 cv: int = 3, scoring=None, *args, **kwargs):
+
+        self._figs_class = figs
+        self.n_rules_list = np.array(n_rules_list)
+        self.cv = cv
+        self.scoring = scoring
+
+    def fit(self, X, y):
+        self.scores_ = []
+        for n_rules in self.n_rules_list:
+            est = self._figs_class(max_rules=n_rules)
+            cv_scores = cross_val_score(est, X, y, cv=self.cv, scoring=self.scoring)
+            mean_score = np.mean(cv_scores)
+            if len(self.scores_) == 0:
+                self.figs = est
+            elif mean_score > np.max(self.scores_):
+                self.figs = est
+
+            self.scores_.append(mean_score)
+        self.figs.fit(X=X, y=y)
+
+    def predict_proba(self, X):
+        return self.figs.predict_proba(X)
+
+    def predict(self, X):
+        return self.figs.predict(X)
+
+    @property
+    def max_rules(self):
+        return self.figs.max_rules
+
+
+class FIGSRegressorCV(FIGSCV):
+    def __init__(self,
+                 n_rules_list: List[int] = [6, 12, 24, 30, 50],
+                 cv: int = 3, scoring='r2', *args, **kwargs):
+        super(FIGSRegressorCV, self).__init__(figs=FIGSRegressor, n_rules_list=n_rules_list,
+                                              cv=cv, scoring=scoring, *args, **kwargs)
+
+
+class FIGSClassifierCV(FIGSCV):
+    def __init__(self,
+                 n_rules_list: List[int] = [6, 12, 24, 30, 50],
+                 cv: int = 3, scoring="accuracy", *args, **kwargs):
+        super(FIGSClassifierCV, self).__init__(figs=FIGSClassifier, n_rules_list=n_rules_list,
+                                               cv=cv, scoring=scoring, *args, **kwargs)
+
+
+if __name__ == '__main__':
+    from sklearn import datasets
+
+    X_cls, Y_cls = datasets.load_breast_cancer(return_X_y=True)
+    # Y_cls = np.column_stack((Y_cls, np.random.permutation(Y_cls)))
+    # X_reg, Y_reg = datasets.make_friedman1(100)
+
+    # est = FIGSRegressorCV()
+    # est.fit(X_reg, Y_reg)
+    # est.predict(X_reg)
+    # print(est.max_rules)
+
+    est = FIGSClassifierCV()
+    est.fit(X_cls, Y_cls)
+    est.predict(X_cls)
+    print(est.max_rules)
