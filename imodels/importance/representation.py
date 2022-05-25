3148323f891c4e28150aee47a907ff768fdfa1b7
Yan Shuo Tan <yanshuo@gmail.com>
2022-05-25 12:09:33 -0700

    Partial refactoring and documentation

--- imodels/importance/representation.py
+++ imodels/importance/representation.py
@@ -7,37 +7,33 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import BaseEnsemble
 
 
-def compare(data, feature, threshold, sign=True):
-    """
-    :param data: A 2D array of size (n_sample, n_feat)
-    :param feature:
-    :param threshold:
-    :param sign:
-    :return:
-    """
-    if sign:
-        return data[:, feature] > threshold
-    else:
-        return data[:, feature] <= threshold
-
-
-def compare_all(data, features, thresholds, signs):
-    return ~np.logical_xor(data[:, features] > thresholds, signs)
-
-
 class LocalDecisionStump:
+    """
+    An object that implements a callable local decision stump function and that also includes some meta-data that
+    allows for an API to interact with other methods in the package.
+
+    A local decision stump is a tri-valued function that is zero outside of a rectangular region, and on that region,
+    takes either a positive or negative value, depending on whether a single designated feature is above or below a
+    threshold. For more information on what a local decision stump is, refer to our paper.
+
+    :param feature: int
+        Feature used in the decision stump
+    :param threshold: float
+        Threshold used in the decision stump
+    :param left_val: float
+        The value taken when x_k <= threshold
+    :param right_val: float
+        The value taken when x_k > threshold
+    :param a_features: list of ints
+        List of ancestor feature indices (ordered from highest ancestor to lowest)
+    :param a_thresholds: list of floats
+        List of ancestor thresholds (ordered from highest ancestor to lowest)
+    :param a_signs: list of bools
+        List of signs indicating whether the current node is in the left child (False) or right child (True) of the
+        ancestor nodes (ordered from highest ancestor to lowest)
+    """
 
     def __init__(self, feature, threshold, left_val, right_val, a_features, a_thresholds, a_signs):
-        """
-
-        :param feature:
-        :param threshold:
-        :param left_val:
-        :param right_val:
-        :param a_features: list, comprising ancestor features
-        :param a_thresholds: list, comprising ancestor thresholds
-        :param a_signs: list, comprising ancestor signs (0: left, 1: right)
-        """
 
         self.feature = feature
         self.threshold = threshold
@@ -49,15 +45,18 @@ class LocalDecisionStump:
 
     def __call__(self, data):
         """
+        Return values of the local decision stump function on an input data matrix with samples as rows
 
-        :param data: A 2D array of query values
-        :return:
+        :param data: array-like of shape (n_samples, n_features)
+            Data matrix to feed into the local decision stump function
+        :return: array-like of shape (n_samples,)
+            Function values on the data
         """
 
-        root_to_stump_path_indicators = compare_all(data, self.a_features, np.array(self.a_thresholds),
-                                                    np.array(self.a_signs))
+        root_to_stump_path_indicators = _compare_all(data, self.a_features, np.array(self.a_thresholds),
+                                                     np.array(self.a_signs))
         in_node = np.all(root_to_stump_path_indicators, axis=1).astype(int)
-        is_right = compare(data, self.feature, self.threshold).astype(int)
+        is_right = _compare(data, self.feature, self.threshold).astype(int)
         result = in_node * (is_right * self.right_val + (1 - is_right) * self.left_val)
 
         return result
@@ -70,15 +69,26 @@ class LocalDecisionStump:
 
 def make_stump(node_no, tree_struct, parent_stump, is_right_child, normalize=False):
     """
-    Create a single localized decision stump corresponding to a node
+    Create a single local decision stump corresponding to a node in a scikit-learn tree structure object.
+    The nonzero values of the stump are chosen so that the vector of local decision stump values over the training
+    set (used to fit the tree) is orthogonal to those of all ancestor nodes.
 
-    :param node_no:
-    :param tree_struct:
-    :param parent_stump:
-    :param is_right_child:
-    :param normalize:
-    :return:
+    :param node_no: int
+        The index of the node
+    :param tree_struct: object
+        The scikit-learn tree object
+    :param parent_stump: LocalDecisionStump object
+        The local decision stump corresponding to the parent of the node in question
+    :param is_right_child: bool
+        True if the new node is the right child of the parent node, False otherwise
+    :param normalize: bool
+        Flag. If set to True, then divide the nonzero function values by sqrt(n_samples in node) so that the
+        vector of function values on the training set has unit norm. If False, then do not divide, so that the
+        vector of function values on the training set has norm equal to n_samples in node.
+    :return: LocalDecisionStump object
+        The local decision stump corresponding to the node in question
     """
+    # Get features, thresholds and signs for ancestors
     if parent_stump is None:  # If root node
         a_features = []
         a_thresholds = []
@@ -87,38 +97,46 @@ def make_stump(node_no, tree_struct, parent_stump, is_right_child, normalize=Fal
         a_features = parent_stump.a_features + [parent_stump.feature]
         a_thresholds = parent_stump.a_thresholds + [parent_stump.threshold]
         a_signs = parent_stump.a_signs + [is_right_child]
-
-    feature = tree_struct.feature[node_no]
-    threshold = tree_struct.threshold[node_no]
-    parent_size = tree_struct.n_node_samples[node_no]
+    # Get indices for left and right children of the node in question
     left_child = tree_struct.children_left[node_no]
     right_child = tree_struct.children_right[node_no]
+    # Get quantities relevant to the node in question
+    feature = tree_struct.feature[node_no]
+    threshold = tree_struct.threshold[node_no]
     left_size = tree_struct.n_node_samples[left_child]
     right_size = tree_struct.n_node_samples[right_child]
-    if not normalize:
-        # return LocalDecisionStump(feature, threshold, -1, 1, a_features, a_thresholds, a_signs)
-        left_val = - np.sqrt(right_size / left_size)
-        right_val = np.sqrt(left_size / right_size)
-    else:
-        left_val = - np.sqrt(right_size / (left_size * parent_size))
-        right_val = np.sqrt(left_size / (right_size * parent_size))
+    parent_size = tree_struct.n_node_samples[node_no]
+    normalization = parent_size if normalize else 1
+    left_val = - np.sqrt(right_size / (left_size * normalization))
+    right_val = np.sqrt(left_size / (right_size * normalization))
+
     return LocalDecisionStump(feature, threshold, left_val, right_val, a_features, a_thresholds, a_signs)
 
 
 def make_stumps(tree_struct, normalize=False):
     """
-    Take sklearn decision tree structure and create a collection of local
-    decision stump objects
+    Create a collection of local decision stumps corresponding to all internal nodes in a scikit-learn tree structure
+    object.
 
-    :param tree_struct:
-    :param normalize:
-    :return: list of stumps
+    :param tree_struct: object
+        The scikit-learn tree object
+    :param normalize: bool
+        Flag. If set to True, then divide the nonzero function values by sqrt(n_samples in node) so that the
+        vector of function values on the training set has unit norm. If False, then do not divide, so that the
+        vector of function values on the training set has norm equal to n_samples in node.
+    :return:
+        stumps: list of LocalDecisionStump objects
+            The local decision stumps corresponding to all internal node in the tree structure
+        num_splits_per_feature: array-like of shape (n_features,)
+            The number of splits in the tree on each original feature
     """
     stumps = []
     num_splits_per_feature = [0] * tree_struct.n_features
 
     def make_stump_iter(node_no, tree_struct, parent_stump, is_right_child, normalize, stumps, num_splits_per_feature):
-
+        """
+        Helper function for iteratively making local decision stump objects and appending them to the list stumps.
+        """
         new_stump = make_stump(node_no, tree_struct, parent_stump, is_right_child, normalize)
         stumps.append(new_stump)
         num_splits_per_feature[new_stump.feature] += 1
@@ -143,16 +161,21 @@ def tree_feature_transform(stumps, X):
 
 
 class TreeTransformer(TransformerMixin, BaseEstimator):
+    """
+    A transformer that transforms data using a representation built from local decision stumps from a tree or tree
+    ensemble. The transformer also comes with meta data on the local decision stumps and methods that allow
+    for transformations using subrepresentations corresponding to each of the original features.
+
+    :param estimator: scikit-learn estimator
+        The scikit-learn tree or tree ensemble estimator object
+    :param max_components_type: {"median", "max", "}
+        Method for choosing the number of components for PCA. Can be either "median", "max",
+        or a fraction in [0, 1]. If "median" (respectively "max") then this is set as the median (respectively max
+        number of splits on that feature in the RF. If a fraction, then this is set to be the fraction * n
+    :param normalize:
+    """
 
     def __init__(self, estimator, max_components_type="median", fraction_chosen=1.0, normalize=False):
-        """
-
-        :param estimator:
-        :param max_components_type: Method for choosing the number of components for PCA. Can be either "median", "max",
-            or a fraction in [0, 1]. If "median" (respectively "max") then this is set as the median (respectively max
-            number of splits on that feature in the RF. If a fraction, then this is set to be the fraction * n
-        :param normalize:
-        """
         self.estimator = estimator
         self.max_components_type = max_components_type
         self.fraction_chosen = fraction_chosen
@@ -162,12 +185,10 @@ class TreeTransformer(TransformerMixin, BaseEstimator):
             self.all_stumps = []
             for tree_model in estimator.estimators_:
                 tree_stumps, num_splits_per_feature = make_stumps(tree_model.tree_, normalize)
-                self.all_stumps += tree_stumps  # make_stumps(tree_model.tree_, normalize)
+                self.all_stumps += tree_stumps
                 num_splits_per_feature_all.append(num_splits_per_feature)
-
         else:
-            tree_stumps, num_splits_per_feature = make_stumps(estimator.tree_,
-                                                              normalize)  # make_stumps(estimator.tree_, normalize)
+            tree_stumps, num_splits_per_feature = make_stumps(estimator.tree_, normalize)
             self.all_stumps = tree_stumps
             num_splits_per_feature_all.append(num_splits_per_feature)
         self.original_feat_to_stump_mapping = defaultdict(list)
@@ -175,13 +196,10 @@ class TreeTransformer(TransformerMixin, BaseEstimator):
             self.original_feat_to_stump_mapping[stump.feature].append(idx)
         self.pca_transformers = defaultdict(lambda: None)
         self.original_feat_to_transformed_mapping = defaultdict(list)
-        # median_splits_per_feature = [list(col) for col in zip(*median_splits_per_feature)]
-        # self.median_splits = [statistics.median(median_splits_per_feature[i]) for i in
-        #                       range(len(median_splits_per_feature))]
         self.median_splits = np.median(num_splits_per_feature_all, axis=0)
         self.max_splits = np.max(num_splits_per_feature_all, axis=0)
 
-    def fit(self, X, y=None, parallelize=False, always_pca=True):
+    def fit(self, X, y=None, always_pca=True):
 
         def pca_on_stumps(k):
             restricted_stumps = [self.all_stumps[idx] for idx in self.original_feat_to_stump_mapping[k]]
@@ -222,20 +240,11 @@ class TreeTransformer(TransformerMixin, BaseEstimator):
             return pca, n_new_feats
 
         n_orig_feats = X.shape[1]
-        if parallelize:
-            results = Parallel(n_jobs=8)(delayed(pca_on_stumps)(k) for k in np.arange(n_orig_feats))
-            counter = 0
-            for k in np.arange(n_orig_feats):
-                self.pca_transformers[k], n_new_feats_for_k = results[k]
-                self.original_feat_to_transformed_mapping[k] = np.arange(counter, counter + n_new_feats_for_k)
-                counter += n_new_feats_for_k
-
-        else:
-            counter = 0
-            for k in np.arange(n_orig_feats):
-                self.pca_transformers[k], n_new_feats_for_k = pca_on_stumps(k)
-                self.original_feat_to_transformed_mapping[k] = np.arange(counter, counter + n_new_feats_for_k)
-                counter += n_new_feats_for_k
+        counter = 0
+        for k in np.arange(n_orig_feats):
+            self.pca_transformers[k], n_new_feats_for_k = pca_on_stumps(k)
+            self.original_feat_to_transformed_mapping[k] = np.arange(counter, counter + n_new_feats_for_k)
+            counter += n_new_feats_for_k
 
     def transform(self, X):
         transformed_feature_vectors_sets = []
@@ -270,13 +279,39 @@ class TreeTransformer(TransformerMixin, BaseEstimator):
                 transformed_feature_vectors = self.pca_transformers[k].transform(transformed_feature_vectors)
         return transformed_feature_vectors
 
-    def get_transformed_X_for_feat(self, X_transformed, feature, max_components):
-        '''
-        This method takes in the transformed X matrix (applying the node basis) and returns the X matrix restricted to a
-        particular feature.
-        '''
-        X_transformed_feat = X_transformed[:, self.original_feat_to_transformed_mapping[feature]]
-        if max_components <= X_transformed_feat.shape[1]:
-            return X_transformed[:, self.original_feat_to_transformed_mapping[feature]]
-        else:
-            return X_transformed[:, self.original_feat_to_stump_mapping[feature]]
+
+def _compare(data, k, threshold, sign=True):
+    """
+    Obtain indicator vector for the samples with k-th feature > threshold
+
+    :param data: array-like of shape (n_sample, n_feat)
+    :param k: int
+        Index of feature in question
+    :param threshold: float
+        Threshold for the comparison
+    :param sign: bool
+        Flag, if False, return indicator of the complement
+    :return: array-like of shape (n_samples,)
+    """
+    if sign:
+        return data[:, k] > threshold
+    else:
+        return data[:, k] <= threshold
+
+
+def _compare_all(data, ks, thresholds, signs):
+    """
+    Obtain indicator vector for the samples with k-th feature > threshold or <= threshold (depending on sign)
+    for all k in ks
+
+    :param data: array-like of shape (n_sample, n_feat)
+    :param ks: list of ints
+        Indices of feature in question
+    :param thresholds: list of floats
+        Threshold for the comparison
+    :param signs: list of bools
+        Flags, if k-th element if True, then add the condition k-th feature > threshold, otherwise add the
+        condition k-th feature <= threshold
+    :return:
+    """
+    return ~np.logical_xor(data[:, ks] > thresholds, signs)
