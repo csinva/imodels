9b9aa1451cddf3ac5d469684442f2d689e028bdf
aagarwal1996 <aa3797@berkeley.edu>
2022-06-08 19:41:44 -0700

    adding updated init file

--- imodels/importance/__init__.py
+++ imodels/importance/__init__.py
@@ -4,5 +4,5 @@ Feature importance methods for black box models
 
 from .representation import TreeTransformer
 from .r2f import R2F
-from .r2f_experimental import R2FExp,GeneralizedMDI,LassoScorer,RidgeScorer,ElasticNetScorer
+from .r2f_experimental import R2FExp,GeneralizedMDI,GeneralizedMDIJoint,LassoScorer,RidgeScorer,ElasticNetScorer,RobustScorer,LogisticScorer,JointRidgeScorer,JointLogisticScorer,JointRobustScorer
 from .LassoICc import LassoLarsICc
