fixing normalization for gcv error metric

imodels/importance/r2f_experimental.py
@@ -580,8 +580,8 @@ class JointRidgeScorer(JointScorerBase, ABC):
                 restricted_preds = restricted_feats @ restricted_coefs + ridge_model.intercept_
                 if self.metric == "gcv":
                     best_alpha_index = np.where(ridge_model.alphas == ridge_model.alpha_)[0][0]
-                    LOO_error = np.sum(ridge_model.cv_values_[:,best_alpha_index])
-                    R2 = 1 - (LOO_error/np.var(y))
+                    LOO_error = np.sum(ridge_model.cv_values_[:,best_alpha_index])/len(y)
+                    R2 = 1.0 - (LOO_error/np.var(y))
                     self.scores[k] = R2
                 else:
                     self.scores[k] = self.metric(y_test, restricted_preds)