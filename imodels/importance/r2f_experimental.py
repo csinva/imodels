fix bug in r2 computation
* divide by var(y) instead of norm(y)^2

imodels/importance/r2f_experimental.py
@@ -643,10 +643,8 @@ class JointRidgeScorer(JointScorerBase, ABC):
 
             if multi_class:
                 looe = _get_partial_model_looe_multiclass(X_test, y_test_onehot, start_indices, ridge_model.alpha_, ridge_model.coef_)
-                y_norm_sq = X.shape[0]  # i think this is where y-mean comes in. should be variance
-                # y_norm_sq = np.linalg.norm(y_onehot, axis=0)**2
                 for k in range(len(start_indices) - 1):
-                    R2 = 1 - np.sum(looe[:, k, :] ** 2, axis=0) / y_norm_sq
+                    R2 = 1 - np.sum(looe[:, k, :] ** 2, axis=0) / np.var(y_onehot, axis=0)
                     self.scores[k] = np.sum(R2 * (y_onehot == 1).mean(axis=0))
                     self.class_scores[k] = dict(zip(self.classes, R2))
                 else:
@@ -654,9 +652,8 @@ class JointRidgeScorer(JointScorerBase, ABC):
                     self.class_scores[k] = dict(zip(self.classes, np.zeros(len(self.classes))))
             else:
                 looe = _get_partial_model_looe(X_test, y_test, start_indices, ridge_model.alpha_, ridge_model.coef_)
-                y_norm_sq = np.linalg.norm(y) ** 2
                 for k in range(len(start_indices) - 1):
-                    self.scores[k] = 1 - np.sum(looe[:, k] ** 2) / y_norm_sq
+                    self.scores[k] = 1 - np.sum(looe[:, k] ** 2) / np.var(y)
                 else:
                     self.scores[k] = 0
         else: