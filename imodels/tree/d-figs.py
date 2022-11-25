7a8be769d87112706bf80711fe7b40f2e5924935
Jerry Jia <jerry.jia0625@gmail.com>
2022-11-25 13:01:55 -0800

    different phase fit and excludes NaN samples

--- imodels/tree/d-figs.py
+++ imodels/tree/d-figs.py
@@ -1,3 +1,4 @@
+import copy
 from copy import deepcopy
 from typing import List
 
@@ -21,13 +22,13 @@ from imodels import FIGSRegressor
 class D_FIGS(FIGSRegressor):
     # Needs to store the old X and y
 
+    # feature_phases = {1 : (X, y, model), 2 : (X_phase2, y, model)}
     feature_phases = None
 
     def __init__(self, max_rules: int = 12, min_impurity_decrease: float = 0.0, random_state=None,
-                 max_features: str = None):
+                 max_features: str = None, feature_phases={}):
         super().__init__(max_rules, min_impurity_decrease, random_state, max_features)
-        # self.feature_phases = feature_phases
-        # , feature_phases = {}
+        self.feature_phases = feature_phases
 
     def check_phase(self, old_phases, new_phase):
         for i in range(len(old_phases)):
@@ -40,7 +41,7 @@ class D_FIGS(FIGSRegressor):
     '''
     add the new phase features to X, delete samples that has NaN in new_phase potentially refit the model?
     '''
-
+    '''
     def add_new_phase(self, new_phase):
         self.check_phase(self.old_phase, new_phase)
         concatenated_phase = np.concatenate((self.old_phase, new_phase), axis=0)
@@ -53,22 +54,55 @@ class D_FIGS(FIGSRegressor):
                 new_feature = new_phase[node.idx[i]]  # new phase features for the particular sample i
                 if not np.isnan(new_feature).any():  # If the new phase has no nan
                     new_idx.append(node.idx[i])
-            node.idx = new_idx  # The leaves that we can potentially split on now contain only samples with new_phase
+            node.idx = new_idx  # The leaves that we can potentially split on now contain only samples with new_phase'''
 
-    def extend_trees(self, X, y, max_rules=5):
+    def fit_phase_1(self, X, y):
+        self.fit(X, y)
+        # Store a deep copy of the whole model for easier prediction use in the future
+        self.feature_phases[1] = (X, y, deepcopy(self))
+        return self
+
+    def fit_phase_n(self, X, y, max_rules=15):
+        phase_idx = len(self.feature_phases)  # infer the number of phase from the dict
+        prev_phase = self.feature_phases[phase_idx][0]
+        new_phase = X[:, len(prev_phase[0]):]
+        all_leaves = []
+        for node in self.trees_:
+            all_leaves += self.get_leaves(node)
+        # print(self.feature_phases[phase_idx])
+        # print(new_phase)
+        # Right now, we are only removing samples that do not have the newest phase in the leaves, not their ancestors
+        for node in all_leaves:
+            for i in range(len(node.idxs)):
+                if node.idxs[i]:
+                    new_feature = new_phase[i]
+                    # new phase features for the particular sample i
+                    # If the new phase has nan, which means it is not valid and should be false in the idxs
+                    if np.isnan(new_feature).any():
+                        node.idxs[i] = False
+
+        self.extend_trees(X, y, all_leaves, max_rules=max_rules)
+        self.feature_phases[phase_idx + 1] = (X, y, deepcopy(self))
+        return self
+
+    def extend_trees(self, X, y, all_leaves, max_rules=15):
         # Need to add max_rules each time so that it's bigger than the complexity
         self.max_rules += max_rules
-        all_leaves = []
         potential_splits = []
         y_predictions_per_tree = {}  # predictions for each tree
         y_residuals_per_tree = {}  # based on predictions above
+        first_extend = True
 
         # Get all the leaves from the previous model
-        for node in self.trees_:
-            all_leaves += self.get_leaves(node)
+        # for node in self.trees_:
+        #    all_leaves += self.get_leaves(node)
         # iterate through all the leaves and split them on the new feature
         for leaf in all_leaves:
-            potential_split = self._construct_node_with_stump(X, y, idxs=leaf.idxs, tree_num=leaf.tree_num, max_features=None)
+            # b = np.isnan(X[leaf.idxs]).any()
+            if len(X[leaf.idxs]) == 0:
+                continue
+            potential_split = self._construct_node_with_stump(X, y, idxs=leaf.idxs, tree_num=leaf.tree_num,
+                                                              max_features=None)
             if potential_split.impurity_reduction is not None:
                 # Update the leaves on the previous model
                 leaf.setattrs(feature=potential_split.feature,
@@ -78,11 +112,12 @@ class D_FIGS(FIGSRegressor):
                               right_temp=potential_split.right_temp,
                               tree_num=potential_split.tree_num,
                               impurity=potential_split.impurity,
-                              idx=potential_split.idxs)
+                              idxs=potential_split.idxs)
                 # Add to the potential splits, and do the same fitting process as in the fig
                 potential_splits.append(leaf)
-        for i in potential_splits:
-            print(i.impurity_reduction)
+
+        # for i in potential_splits:
+        #    print(i.impurity_reduction)
         potential_splits = sorted(potential_splits, key=lambda x: x.impurity_reduction)
         finished = False
         while len(potential_splits) > 0 and not finished:
@@ -99,7 +134,6 @@ class D_FIGS(FIGSRegressor):
 
             # if added a tree root
             if split_node.is_root:
-
                 # start a new tree
                 self.trees_.append(split_node)
 
@@ -109,7 +143,37 @@ class D_FIGS(FIGSRegressor):
                         node_.tree_num = len(self.trees_) - 1
 
                 # add new root potential node
-                node_new_root = Node(is_root=True, idxs=np.ones(X.shape[0], dtype=bool),
+                phase_idx = len(self.feature_phases)  # infer the number of phase from the dict
+                prev_phase = self.feature_phases[phase_idx][0]
+                new_phase = X[:, len(prev_phase[0]):]
+                cur_idxs = np.ones(X.shape[0], dtype=bool)
+                for i in range(len(cur_idxs)):
+                    if cur_idxs[i]:
+                        new_feature = new_phase[i]
+                        # new phase features for the particular sample i
+                        # If the new phase has nan, which means it is not valid and should be false in the idxs
+                        if np.isnan(new_feature).any():
+                            cur_idxs[i] = False
+                node_new_root = Node(is_root=True, idxs=cur_idxs,
+                                     tree_num=-1)
+                potential_splits.append(node_new_root)
+
+            if first_extend:
+                first_extend = False
+                # Since we consider about deleting samples with na features,
+                # we should do the same for the new node
+                phase_idx = len(self.feature_phases)  # infer the number of phase from the dict
+                prev_phase = self.feature_phases[phase_idx][0]
+                new_phase = X[:, len(prev_phase[0]):]
+                cur_idxs = np.ones(X.shape[0], dtype=bool)
+                for i in range(len(cur_idxs)):
+                    if cur_idxs[i]:
+                        new_feature = new_phase[i]
+                        # new phase features for the particular sample i
+                        # If the new phase has nan, which means it is not valid and should be false in the idxs
+                        if np.isnan(new_feature).any():
+                            cur_idxs[i] = False
+                node_new_root = Node(is_root=True, idxs=cur_idxs,
                                      tree_num=-1)
                 potential_splits.append(node_new_root)
 
@@ -215,4 +279,53 @@ class D_FIGS(FIGSRegressor):
         return s2
 
 
+if __name__ == '__main__':
+    '''
+    # Data generating function
+    # 1[X0 < 0.5 and X1 < 0.5] + 1[X2 < 0.5 and X3 < 0.5]
+    X_fig_large = np.random.binomial(1, 0.5, (20000, 4))
+    y_fig_large = [0.0] * 20000
+    for idx in range(len(X_fig_large)) :
+        x1_x2 = X_fig_large[idx][0] < 0.5 and X_fig_large[idx][1] < 0.5
+        x3_x4 = X_fig_large[idx][2] < 0.5 and X_fig_large[idx][3] < 0.5
+        if x1_x2 and x3_x4:
+            y_fig_large[idx] = 2.0
+        elif x1_x2 or x3_x4:
+            y_fig_large[idx] = 1.0
+    X_fig_large_1 = X_fig_large[:, :2]
 
+    clf = D_FIGS(max_rules=4)
+
+    # First time use fit phase_1
+    clf.fit_phase_1(X_fig_large_1, np.array(y_fig_large))
+
+    # Use fit phase_n in the later phase
+    clf.fit_phase_n(X_fig_large, np.array(y_fig_large), max_rules=6)'''
+
+    # Data generating function
+    # 1[X0 < 0.5 and X1 < 0.5] + 1[X2 < 0.5 and X3 < 0.5]
+    # This is uniform since NaN is Float
+    X_fig_large_na = np.random.uniform(0, 1, (20000, 4))
+    y_fig_large_na = [0.0] * 20000
+    for idx in range(len(X_fig_large_na)):
+        x1_x2 = X_fig_large_na[idx][0] < 0.5 and X_fig_large_na[idx][1] < 0.5
+        x3_x4 = X_fig_large_na[idx][2] < 0.5 and X_fig_large_na[idx][3] < 0.5
+        prob = np.random.uniform(0, 1)
+        if prob <= 0.2:
+            X_fig_large_na[idx][2] = np.nan
+            X_fig_large_na[idx][3] = np.nan
+            x3_x4 = False
+        if x1_x2 and x3_x4:
+            y_fig_large_na[idx] = 2.0
+        elif x1_x2 or x3_x4:
+            y_fig_large_na[idx] = 1.0
+    X_fig_large_1_na = X_fig_large_na[:, :2]
+    if (np.isnan(X_fig_large_na).any()):
+        print("The later phase has missing values, (some samples' features are nan)")
+    clf1 = D_FIGS(max_rules=4)
+    clf1.fit_phase_1(X_fig_large_1_na, np.array(y_fig_large_na))
+    clf1.fit_phase_n(X_fig_large_na, np.array(y_fig_large_na), max_rules=8)
+    print(clf1.predict(
+        [[1, 1, 1, 1], [1, 0, 0, 0], [1, 1, 0, 0], [0, 0, 0, 0], [0, 0, 1, 1], [0, 0, 1, 0], [1, 0, 1, 0]]))
+    print("")
+    print("Correct answer is" + "[0, 1, 1, 2, 1, 1, 0]")
