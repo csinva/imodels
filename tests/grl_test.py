d9fe600bb901bc5a3d5f0aa32e5e3f7183c9ff8b
Chandan Singh <chandan_singh@berkeley.edu>
2022-12-06 12:07:33 -0800

    bumpy github-actions versions

--- tests/grl_test.py
+++ tests/grl_test.py
@@ -31,15 +31,13 @@ class TestGRL(unittest.TestCase):
 def test_breast_cancer():
     np.random.seed(13)
     X, Y = sklearn.datasets.load_breast_cancer(as_frame=True, return_X_y=True)
-
     model = GreedyRuleListClassifier(max_depth=10)
-
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
     model.fit(X_train, y_train, feature_names=X_train.columns)
     y_pred = model.predict(X_test)
-    score = accuracy_score(y_test.values,y_pred)
-    print('Accuracy:', score)
-    model._print_list()
+    # score = accuracy_score(y_test.values,y_pred)
+    # print('Accuracy:', score)
+    # model._print_list()
 
 if __name__ == '__main__':
     test_breast_cancer()
