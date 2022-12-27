8abeb0545104eb752f600617411b6b59fd2fdde7
Yan Shuo Tan <yanshuo@gmail.com>
2022-12-27 22:42:07 +0800

    Remove unnecessary files

--- imodels/importance/__init__.py
+++ imodels/importance/__init__.py
@@ -2,4 +2,7 @@
 Feature importance methods for black box models
 """
 
-
+from .gmdi import GMDI, GmdiHelper
+from .ppms import RidgePPM, LogisticPPM, RobustPPM, GlmPPM, GenericPPM
+from .block_transformers import IdentityTransformer, TreeTransformer, \
+    CompositeTransformer, GmdiDefaultTransformer
