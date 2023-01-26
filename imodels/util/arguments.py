dc5f151f38e693dd80115183f1e56ca46caf7815
Chandan Singh <chandan_singh@berkeley.edu>
2023-01-26 14:32:07 -0800

    additional argument checking

--- imodels/util/arguments.py
+++ imodels/util/arguments.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.base import ClassifierMixin
-from sklearn.utils.validation import check_X_y
+from sklearn.utils.validation import check_X_y, check_array
 from sklearn.utils.multiclass import check_classification_targets
 import scipy.sparse
 
@@ -29,4 +29,10 @@ def check_fit_arguments(model, X, y, feature_names):
     y = y.astype(float)
     return X, y, model.feature_names_
 
-
+def check_fit_X(X):
+    """Process X argument for fit and predict methods.
+    """
+    if scipy.sparse.issparse(X):
+        X = X.toarray()
+    X = check_array(X)
+    return X
