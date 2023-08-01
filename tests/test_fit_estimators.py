d75bae18a20303e02121de63a4368c83de712007
jak_tkvs <32125829+jckkvs@users.noreply.github.com>
2023-08-01 10:06:45 +0900

    Update test_fit_estimators.py

--- tests/test_fit_estimators.py
+++ tests/test_fit_estimators.py
@@ -22,12 +22,6 @@ classifiers = [
     TaoTreeClassifier(),
 ]
 
-classifiers_for_discretized_X = [
-    BayesianRuleListClassifier(),
-    FPLassoClassifier(),
-    FPSkopeClassifier(),
-]
-
 regressors = [
     SLIMRegressor(),
     BoostedRulesRegressor(),
@@ -38,10 +32,6 @@ regressors = [
     HSTreeRegressor(),
     HSTreeRegressorCV(),
     TaoTreeRegressor(),
-]
-
-regressors_for_discretized_X = [
-    FPLassoRegressor(),
 ]
 
 
@@ -55,58 +45,3 @@ def test_fit_classifier(classifier) -> None:
 def test_fit_regressor(regressor) -> None:
     X, y = make_regression(n_samples=25, n_features=5)
     regressor.fit(X, y)
-
-
-@pytest.mark.parametrize("classifier", classifiers)
-def test_fit_before_print_classifier(classifier) -> None:
-    X, y = make_classification(n_samples=25, n_features=5)
-    print(classifier)
-
-
-@pytest.mark.parametrize("regressor", regressors)
-def test_fit_before_print_regressor(regressor) -> None:
-    X, y = make_regression(n_samples=25, n_features=5)
-    print(regressor)
-
-
-classifiers_fit_pass = [
-    SLIMClassifier(),
-    GreedyRuleListClassifier(),
-    OneRClassifier(),
-    BoostedRulesClassifier(),
-    RuleFitClassifier(),
-    SkopeRulesClassifier(),
-    C45TreeClassifier(),
-    GreedyTreeClassifier(),
-    FIGSClassifier(),
-    FIGSClassifierCV(),
-    HSTreeClassifier(),
-    HSTreeClassifierCV(),
-    TaoTreeClassifier(),
-]
-
-
-regressors_fit_pass = [
-    SLIMRegressor(),
-    BoostedRulesRegressor(),
-    RuleFitRegressor(),
-    GreedyTreeRegressor(),
-    FIGSRegressor(),
-    FIGSRegressorCV(),
-    HSTreeRegressor(),
-    HSTreeRegressorCV(),
-]
-
-
-@pytest.mark.parametrize("classifier", classifiers_fit_pass)
-def test_fit_after_print_classifier(classifier) -> None:
-    X, y = make_classification(n_samples=25, n_features=5)
-    classifier.fit(X, y)
-    print(classifier)
-
-
-@pytest.mark.parametrize("regressor", regressors_fit_pass)
-def test_fit_after_print_regressor(regressor) -> None:
-    X, y = make_regression(n_samples=25, n_features=5)
-    regressor.fit(X, y)
-    print(regressor)
