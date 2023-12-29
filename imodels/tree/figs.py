efd9a468aa41b2fd277ae5cdd3d716b83723eda6
Matthew Epland <matthewepland@gmail.com>
2023-12-29 12:30:09 -0500

    Clean up line breaks

--- imodels/tree/figs.py
+++ imodels/tree/figs.py
@@ -351,9 +351,9 @@ class FIGS(BaseEstimator):
             for tree_num_ in list(range(len(self.trees_))) + [-1]:
                 y_residuals_per_tree[tree_num_] = deepcopy(y)
 
-                # subtract predictions of all other trees. Since the current tree makes a constant prediction over the
-                # node being split, one may ignore its contributions to the residuals without affecting the
-                # impurity decrease.
+                # subtract predictions of all other trees.
+                # Since the current tree makes a constant prediction over the node being split,
+                # one may ignore its contributions to the residuals without affecting the impurity decrease.
                 for tree_num_other_ in range(len(self.trees_)):
                     if not tree_num_other_ == tree_num_:
                         y_residuals_per_tree[tree_num_] -= y_predictions_per_tree[
